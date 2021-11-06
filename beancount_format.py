d0bdb87f8ce666634d0a720c73e47cae663d4879
Ben Blount <ben@bben.us>
2021-11-05 21:47:36 -0700

    Lock currency_column to 90
    This works well for my use. I have some long auto meta that otherwise puts it way too far away.

--- beancount_format.py
+++ beancount_format.py
@@ -7,7 +7,7 @@ def main() -> None:
     for filename in sys.argv[1:]:
         with open(filename, "r") as f:
             content = f.read()
-        formatted_contents = align_beancount(content)
+        formatted_contents = align_beancount(content, currency_column=90)
         if formatted_contents != content:
             with open(filename, "w") as f:
                 f.write(formatted_contents)
